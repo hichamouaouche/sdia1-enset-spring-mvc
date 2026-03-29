d76daf1083e0b9f8c534bc9d61dc6e3b1bd0fc59
hichamouaouche <hichamouaouche@gmail.com>
2026-03-29 20:39:36 +0100

    update

--- src/main/java/hiou/hicham/sdiaensetspringmvc/web/ProductController.java
+++ src/main/java/hiou/hicham/sdiaensetspringmvc/web/ProductController.java
@@ -1,31 +1,46 @@
 package hiou.hicham.sdiaensetspringmvc.web;
 
-import org.springframework.ui.Model;
 import hiou.hicham.sdiaensetspringmvc.entities.Product;
 import hiou.hicham.sdiaensetspringmvc.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class ProductController {
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @GetMapping("/index")
     public String index(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList", products);
         return "products";
     }
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
     @GetMapping("/delete")
-    public String delete(@RequestParam(name = "id") Long id){
+    public String delete(@RequestParam(name = "id") Long id) {
         productRepository.deleteById(id);
         return "redirect:/index";
-
-
     }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
 
 }
